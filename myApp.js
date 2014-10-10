1c04d174abf05dfc27aa50724715da435456c58a
Sergio Barrera <sdbeng@gmail.com>
2014-10-10 09:11:02 -0700

    Get static table with initialNumberLimit - v2

--- myApp.js
+++ myApp.js
@@ -4,6 +4,17 @@
 * Description
 */
 angular.module('myApp', [])
-.controller('MultiplicationCtrl', ['$scope', function($scope){
-	$scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+.controller('MultiplicationCtrl', ['$scope', '$attrs', function($scope, $attrs){
+	// $scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+	//let's declare a function to give users a chance to enter x
+	function populateNumbers(x) {
+		var numbers = [];
+		for (var i = 0; i < x; i++) {
+			numbers[i] = i + 1;
+	};
+	return numbers;
+}
+	$scope.numberLimit = $attrs.initialNumberLimit || 10;
+	$scope.numbers = populateNumbers($scope.numberLimit);
+
 }]);
